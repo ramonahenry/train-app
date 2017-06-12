cb1d091c08543570f6c17555c6a61d81340a109b
Galore Analytics <galore@Galores-MacBook-Pro.local>
2017-06-11 19:24:27 -0700

    update homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,95 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCfWXcRMi-if9o07hCe8qRCio1ucO8zIFc",
+    authDomain: "trainapp-b229b.firebaseapp.com",
+    databaseURL: "https://trainapp-b229b.firebaseio.com",
+    projectId: "trainapp-b229b",
+    storageBucket: "trainapp-b229b.appspot.com",
+    messagingSenderId: "469587587401"
+  };
+  firebase.initializeApp(config);
+
+
+
+var database = firebase.database();
+var currentTime = moment();
+
+//add captured input values to database
+database.ref().on("child_added", function(childsnapshot) {
+
+    var trainName = childsnapshot.val().trainName;
+    var destination = childsnapshot.val().destination;
+    var trainStart = childsnapshot.val().trainStart;
+    var frequency = childsnapshot.val().frequency;
+    var min = childsnapshot.val().min;
+    var nextTrain = childsnapshot.val().nextTrain;
+
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + trainStart + "</td><td>" + frequency + "</td><td>" + nextTrain + "</td><td>" + min + "</td></tr>");
+
+    if (min < 6){
+      $("td").css("color", "red");
+    }
+});
+
+database.ref().on("value", function(snapshot) {
+   
+
+});
+
+//capture form value and push to divs
+
+$("#add-train").on("click", function() {
+
+    var trainName = $("#train-name-input").val().trim();
+    var destination = $("#dest-input").val().trim();
+    var trainStart = $("#train-time-input").val().trim();
+    var frequency = $("#freq-input").val().trim();
+
+    //error handling values
+    if (trainName == "") {
+        alert(' You must enter a train.');
+        return false;
+    }
+    if (destination == "") {
+        alert('You must enter a destination.');
+        return false;
+    }
+    if (trainStart == "") {
+        alert('You must enter a train start time.');
+        return false;
+    }
+    if (frequency == "") {
+        alert('You must enter a frequency.');
+        return false;
+    }
+
+    //Next train calculations using movment
+    var trainStartCalc = moment(trainStart, "hh:mm").subtract("1, years");
+    
+    var difference = currentTime.diff(moment(trainStartCalc), "minutes");
+    var remainder = difference % frequency;
+    var nextTrainTime = frequency - remainder;
+    var nextTrain = moment().add(nextTrainTime, "minutes").format("hh:mm a");
+
+    var newTrain = {
+        trainName: trainName,
+        destination: destination,
+        trainStart: trainStart,
+        frequency: frequency,
+        min: nextTrainTime,
+        nextTrain: nextTrain
+    }
+
+    console.log(newTrain);
+    database.ref().push(newTrain);
+
+    $("#train-name-input").val("");
+    $("#dest-input").val("");
+    $("#train-time-input").val("");
+    $("#freq-input").val("");
+
+    return false;
+});
+
+    
